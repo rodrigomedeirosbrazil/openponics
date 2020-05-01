e872ae6ae2467058811ec915db80918d4c94a3c2
Rodrigo Medeiros <rmedeiros@techsocial.com.br>
2020-05-01 00:42:07 -0300

    fix EC readings

--- src/services/ec.js
+++ src/services/ec.js
@@ -2,19 +2,48 @@ const Serial = require('raspi-serial').Serial;
 
 const raspiInit = require('../utils/raspiInit');
 
+const DELIMITER = Buffer.from('\r\n');
+const SERIAL_OPTIONS = {
+  portId: '/dev/serial0',
+  baudRate: 300
+}
+
 module.exports = async () => {
   await raspiInit()
-  const serial = new Serial()
+  const serial = new Serial(SERIAL_OPTIONS)
   await openSerial(serial)
-  const data = await readSerial(serial)
+  const timeout = setTimeout( () => {
+    throw new Error('Serial timeout');
+  }, 5000)
+  const data = await readLine(serial)
   await closeSerial(serial)
+  clearTimeout(timeout);
   return data;
+}
+
+const readLine = async serial => {
+  let buffer = Buffer.alloc(0)
+  let chunk
+  let chunks =0
+  while (chunks++ < 5) {
+    chunk = await readSerial(serial)
+    buffer = Buffer.concat([buffer, chunk])
+    let position
+    let position2
+    if ((position = buffer.indexOf(DELIMITER)) !== -1 && (position2 = buffer.indexOf(DELIMITER, position + DELIMITER.length)) !== -1) {
+      const line = buffer.slice(position + DELIMITER.length, position2)
+      buffer = buffer.slice(position2 + DELIMITER.length)
+      return line.toString()
+    }
+  }
+  return ''
 }
 
 const openSerial = serial => {
   return new Promise((resolve, reject) => {
     try {
       serial.open(() => {
+        // console.log('Serial openned.')
         resolve();
       });
     } catch (err) {
@@ -27,6 +56,7 @@ const closeSerial = serial => {
   return new Promise((resolve, reject) => {
     try {
       serial.close(() => {
+        // console.log('Serial closed.')
         resolve();
       });
     } catch (err) {
@@ -39,6 +69,7 @@ const readSerial = serial => {
   return new Promise((resolve, reject) => {
     try {
       serial.on('data', (data) => {
+        // console.log('Data received: ', data.toString())
         resolve(data);
       });
     } catch (err) {
