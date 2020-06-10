f35b8994f0d3dc08785d31a8f2d1a0a3d185be28
Rodrigo Medeiros <contato@medeirostec.com.br>
2020-06-10 11:30:40 -0300

    separated comm from worker

--- src/worker.js
+++ src/worker.js
@@ -1,33 +1,31 @@
 require('dotenv').config()
 
-const api = require('./services/api')
 const db = require('./database/connection')
 
-console.log('Connecting to ' + process.env.API_URL);
+const phService = require('./services/ph')
+const ecService = require('./services/ec')
+// TODO: put humidity sensor and air temperature here!
 
-const worker = async () => {
-  const pins = await db('pins').select('*')
-  const sensors = await db('sensorlogs').orderBy('updated_at', 'desc').select('*').first()
+const worker = () => {
+  console.log('Reading sensors...')
 
-  const sendData = {
-    key: process.env.BOX_KEY,
-    pins,
-    sensors
-  }
-
-  try {
-    const res = await api.post(
-      '/boxes/endpoint',
-      sendData
-    );
-
-    if (res.status === 200) {
-      console.log(res.data);
-    }
-
-  } catch (error) {
-    console.log('erro', error);
-  }
+  Promise.all([
+    phService.readPh(),
+    ecService.readEc()
+  ])
+  .then(([readPh, readEc]) => {
+    console.log('Recording values...')
+    db('sensorlogs')
+      .insert({
+        ph: readPh,
+        ec: readEc.electricCondutivity,
+        water_temp: readEc.waterTemperature
+      });
+  })
+  .catch( error => {
+    console.log('Error occurs when read sensors.', error)
+  })
 }
 
-worker();
+worker()
+console.log('Exiting...')
